6329a055f365f20fb0749f3495eebd561a2751dd
xiej2520 <xiej2520@gmail.com>
2026-07-17 22:59:33 -0500

    try to improve build/link times with ccache, ninja, precompiled headers

--- flake.nix
+++ flake.nix
@@ -34,7 +34,9 @@
               }
               {
                 packages = with pkgs; [
+                  ccache
                   cmake
+                  ninja
                   zlib
                 ];
                 shellHook = ''
